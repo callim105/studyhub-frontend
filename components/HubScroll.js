39a652dec6e4ffca4650d4966cb5170d969d284b
christianalim <christianalim1@gmail.com>
2019-09-18 16:47:46 -0500

    fixed ip stuff for fetches

--- components/HubScroll.js
+++ components/HubScroll.js
@@ -1,13 +1,74 @@
 import React, { Component } from 'react'
-import { View, Text, ScrollView, FlatList } from 'react-native';
+import { View, Text, ScrollView, Button, ActivityIndicator } from 'react-native';
+import { Card } from 'react-native-elements'
+
+import Constants from "expo-constants";
+const { manifest } = Constants;
+const uri = `http://${manifest.debuggerHost.split(':').shift()}:3000/hubs`;
+// const uri = 'http://10.198.66.194:3000/hubs'
 
 
 export default class HubScroll extends Component {
+    constructor(props){
+        super(props)
+        this.state = { isLoading: true, hubs: [] }
+    }
+
+    componentDidMount(){
+        console.log(uri)
+        return fetch(uri)
+        .then(res => res.json())
+        .then(data => {
+            this.setState({
+                isLoading: false,
+                hubs: data
+            })
+        })
+        .catch(err => console.error(err))
+    }
+
+    renderStars = (rating) => {
+        let numRating = Number(rating)
+        let stars = []
+        for(let i = 0; i < numRating; i++ ){
+            stars.push('⭐')
+        }
+        return (stars.join(''))
+    }
+
+
     render() {
+
+        if(this.state.isLoading){
+            return(
+                <View style={{flex: 1, padding: 20}}>
+                    <ActivityIndicator />
+                </View>
+            )
+        }
+
         return (
-            <ScrollView>
-                    <Text>Hubs Near You...</Text>
-            </ScrollView>
-        )
+            <View style={{flex: 1}}>
+              <ScrollView contentContainerStyle={{ paddingVertical: 20 }}>
+                    {this.state.hubs.map(({ name, rating, id }) => (
+                        <Card key={id}>
+                            <Text style={{ marginBottom: 10 }}>
+                                {name}
+                            </Text>
+                            <Text>
+                                Rating: {this.renderStars(rating)}
+                            </Text>
+                            <Button
+                            title="View More"
+                            />
+                        </Card>
+                    ))
+                    }
+              </ScrollView>
+            </View>
+        
+          );
     }
 }
+
+
